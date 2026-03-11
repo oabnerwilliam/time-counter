196e24877211962fbdfc4b4fe885eec17968fc5b
oabnerwilliam <abnerwilliam026@gmail.com>
2026-03-11 14:37:07 -0300

    chore: adicionando segundos no contador

--- src/counter/utils/constants.ts
+++ src/counter/utils/constants.ts
@@ -1,9 +1,9 @@
 export const formatTime = (ms: number | null) => {
-    if (!ms) return `00:00`
+    if (!ms) return `00:00:00`
 
     const seconds = Math.floor(ms / 1000)
     const minutes = Math.floor(seconds / 60)
     const hours = Math.floor(minutes / 60)
 
-    return `${hours.toString().padStart(2, '0')}:${(minutes % 60).toString().padStart(2, '0')}`
+    return `${hours.toString().padStart(2, '0')}:${(minutes % 60).toString().padStart(2, '0')}:${(seconds % 60).toString().padStart(2, '0')}`
 }
